3f93ef7bca4e5319750b661dbae9b2bd9c1a1a8d
leojthiek <leojthiek@gmail.com>
2023-08-09 14:21:00 +0530

    checking

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
+    <div>
    <h1>hello this is my next application , come on</h1>
+   <Link href={'/pages/testing'}>
+   <button>testing</button>
+   </Link>
+    </div>
   )
 }
---
+++ app/pages/testing/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './page.module.css'
+
+export default function Testing() {
+  return (
+    <div className={styles.main}>
+         <h2>my style is woring or not</h2>
+    </div>
+  )
+}
